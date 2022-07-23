497cfd6ba3942abc5c09a2e94a19968ba0753834
Apple <apple@Apples-iMac.local>
2022-07-23 16:14:45 +0700

    Build Upcoming Screen

---
+++ Netflix Clone/ViewModels/TitleVIewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleVIewModel.swift
+//  Netflix Clone
+//
+//  Created by Apple on 22/07/2022.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL:String
+}
